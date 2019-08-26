5e485dbdc70cea044e2b82f1b68f36bbb7b8f132
欧顶银 <oudingyin@oudingyindeMacBook-Pro.local>
2019-08-26 23:49:02 +0800

    .

--- src/components/JingdongCoupon.vue
+++ src/components/JingdongCoupon.vue
@@ -7,6 +7,7 @@
         v-for="item of items"
         :key="item.batchId"
       >满{{item.quota}}减{{item.discount}}
+        <date-picker v-model="item.datetime"></date-picker>
         <el-button @click="getCoupon(item)">领取</el-button>
       </li>
     </ul>
@@ -16,17 +17,31 @@
 <script lang="ts">
 import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 import { getPlusQuanpinList, getPlusQuanpin } from "../api";
-@Component
+import DatePicker from "./DatePicker.vue";
+@Component({
+  components: {
+    DatePicker
+  }
+})
 export default class JingdongCoupon extends Vue {
   items = [];
   getCouponList() {
-    getPlusQuanpinList().then(item => {
-      this.items = item;
+    getPlusQuanpinList().then(items => {
+      this.items = items.map(item => {
+        item.datetime = "";
+        return item;
+      });
       this.$notify.success("列表获取成功");
     });
   }
 
-  getCoupon(data) {
+  async getCoupon(data) {
+    if (data.datetime) {
+      console.log(data.datetime, "抢优惠券");
+      await new Promise(resolve =>
+        setTimeout(resolve, new Date(data.datetime).getTime() - Date.now())
+      );
+    }
     getPlusQuanpin(data).then(({ resultCode }) => {
       if (resultCode === "1000") {
         this.$notify.success("领取成功");
