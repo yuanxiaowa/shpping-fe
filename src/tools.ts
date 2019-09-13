5793adc7a265c1736eb1b9909c3fda4714ca9fcb
oudingyin <oudingyin@mofangge.com>
2019-09-13 16:43:03 +0800

    。

--- src/tools.ts
+++ src/tools.ts
@@ -5,7 +5,7 @@ import { Platform } from "./handlers";
  * @Author: oudingyin
  * @Date: 2019-08-26 09:17:50
  * @LastEditors: oudingy1in
- * @LastEditTime: 2019-09-13 00:32:26
+ * @LastEditTime: 2019-09-13 09:53:12
  */
 interface Ret {
   action: string;
@@ -136,8 +136,12 @@ export function resolveText(text: string, datetime?: string) {
           expectedPrice = 20;
         }
       }
-      action = "coudan";
-      diejia = quote;
+      if (!/\d+点/.test(text)) {
+        action = "coudan";
+        diejia = quote;
+      } else {
+        action = "qiangquan";
+      }
     } else if (
       /前\d+(?!分钟)|(?<!\d)0\.\d+|速度|抽奖|领金豆|淘宝搜|(?<!可用|消灭)(小|聚划算)?红包|虹包|神价|秒杀|神车|手慢无|手快有|好价|神价/.test(
         text
